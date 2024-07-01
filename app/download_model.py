89392773240ce9987b635f3cd7c11db440160959
Brahiam <starsmasters123@gmail.com>
2024-07-01 15:22:50 -0400

    Dockerfile model downloading issues

--- app/download_model.py
+++ app/download_model.py
@@ -3,7 +3,10 @@ import os
 
 def download_model():
     file_id = os.environ.get('GDRIVE_FILE_ID')
-    url = f'https://drive.google.com/uc?id={file_id}'
+    if not file_id:
+        raise ValueError("GDRIVE_FILE_ID environment variable not set or empty")
+    print(f"File ID: {file_id}")
+    url = f'https://drive.google.com/uc?id={file_id}&export=download'
     output = 'complete_model.h5'
     gdown.download(url, output, quiet=False)
 
