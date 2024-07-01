8b0550b3581e4bec5c181bf2387c8be63d8202c3
Brahiam <starsmasters123@gmail.com>
2024-07-01 16:18:15 -0400

    TCP check on port 8000

--- app/server.py
+++ app/server.py
@@ -50,4 +50,4 @@ def predict():
     return jsonify({'predictions': result})
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000)
